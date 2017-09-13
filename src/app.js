e8420e1fc4aae0e30030724311cab8691a67ae8e
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-09-12 22:01:45 -0700

    add react router

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './styles/styles.scss';
 
-ReactDOM.render()
+ReactDOM.render(<p>Welcome to Expensift</p>, document.getElementById('app'));
